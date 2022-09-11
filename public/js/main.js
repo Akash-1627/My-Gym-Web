793274793e82fe194b54cde83a194231dc788fd2
Akash1627 <akash1627.be21@chitkara.edu.in>
2022-09-12 00:54:41 +0530

    all files added and committed

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+document.onscroll = function() {scroll()};
+
+function scroll(){
+    if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
+        document.getElementById('navbar').style.background = "#111";
+    }
+    else{
+        document.getElementById('navbar').style.background = "none";
+    }
+}
