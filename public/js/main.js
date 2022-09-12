39fb16e82c706a2b1e97f154007d38b56728ac43
Akash1627 <akash1627.be21@chitkara.edu.in>
2022-09-12 22:17:48 +0530

    all files added and updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
-document.onscroll = function() {scroll()};
+// document.onscroll = function() {scroll()};
 
-function scroll(){
-    if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
-        document.getElementById('navbar').style.background = "#111";
-    }
-    else{
-        document.getElementById('navbar').style.background = "none";
-    }
-}
+// function scroll(){
+//     if(document.body.scrollTop>20 || document.documentElement.scrollTop>20){
+//         document.getElementById('navbar').style.background = "#111";
+//     }
+//     else{
+//         document.getElementById('navbar').style.background = "none";
+//     }
+// }
